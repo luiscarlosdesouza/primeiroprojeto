9e9b2ceb2d6476d35e451e7054f3449d06e53432
php 33 <php.33@metralha.cec.ime.usp.br>
2017-02-13 22:18:09 -0200

    primeiro commit

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+
+  public function ola()
+  {
+    return view('teste.ola');
+
+  }
+
+
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Página de saudação</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+
+  </head>
+  <body>
+
+    <div class="container">
+      <h2>Introdução ao Desenvolvimento de Sistemas Web com PHP</h2>
+      <h4>Curso Laravel PHP</h4>
+    </div>
+    <script type="text/javascript" src="/js/app.js">
+  </body>
+
+</html>
